70b911483760f06ac0e54e502e227847ae00dbc1
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-09-13 15:41:56 +0600

    display dynamicaly cove img title author author img reading time

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,17 +1,31 @@
 import PropTypes from 'prop-types';
-import { prototype } from 'postcss/lib/previous-map';
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+    // console.log(blog);
+    const { Title, Cover, Author_img, Author, Reading_Time, Posted_Date, Hashtag } = blog;
     return (
         <div>
-
+            <img src={Cover} alt="" />
+            <div className='flex justify-between'>
+                <div className='flex items-center'>
+                    <img className='w-14 h-14 rounded-full' src={Author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{Author}</h3>
+                        <p>{Posted_Date}</p>
+                    </div>
+                </div>
+                <p>{Reading_Time}.min read</p>
+            </div>
+            <h2 className="text-4xl">{Title}</h2> <br />
+            <p>
+            Hashtag: {blog.Hashtag}
+            </p>
         </div>
     );
 };
 
 
-Blog.PropTypes ={
-    blog:PropTypes.object.isRequired
+Blog.prototype = {
+    blog: PropTypes.object.isRequired
 }
 export default Blog;
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,4 @@
-import { data } from "autoprefixer";
+import Blog from "../Blog/Blog";
 import { useEffect } from "react";
 import { useState } from "react";
 
@@ -12,6 +12,9 @@ const Blogs = () => {
     return (
         <div className="w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
+            {
+               blogs.map(blog => <Blog key={blog.ID} blog ={blog}></Blog>)
+            }
         </div>
     );
 };
